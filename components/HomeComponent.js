f77684d0080910e340838e9d42dae3bd02c61c8b
Andry Mironov <andry.mironov94@gmail.com>
2019-04-04 15:47:45 +0300

    Week 2. Activity Indicator

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -3,6 +3,8 @@ import { ScrollView, View, Text } from 'react-native'
 import { Card } from 'react-native-elements'
 import { connect } from 'react-redux'
 
+import { Loading } from './LoadingComponent'
+
 import { baseUrl } from '../shared/baseUrl'
 
 const mapStateToProps = state => {
@@ -17,18 +19,28 @@ const mapStateToProps = state => {
 function RenderItem(props) {
 	const item = props.item
 
-	if (item != null) {
+	if (props.isLoading) {
+		return <Loading />
+	} else if (props.errMess) {
 		return (
-			<Card
-				featuredTitle={item.name}
-				featuredSubtitle={item.designation}
-				image={{ uri: baseUrl + item.image }}
-			>
-				<Text style={{ margin: 10 }}>{item.description}</Text>
-			</Card>
+			<View>
+				<Text> {props.errMess} </Text>
+			</View>
 		)
 	} else {
-		return <View />
+		if (item != null) {
+			return (
+				<Card
+					featuredTitle={item.name}
+					featuredSubtitle={item.designation}
+					image={{ uri: baseUrl + item.image }}
+				>
+					<Text style={{ margin: 10 }}>{item.description}</Text>
+				</Card>
+			)
+		} else {
+			return <View />
+		}
 	}
 }
 
@@ -42,12 +54,18 @@ class Home extends Component {
 			<ScrollView>
 				<RenderItem
 					item={this.props.dishes.dishes.filter(dish => dish.featured)[0]}
+					isLoading={this.props.dishes.isLoading}
+					erreMess={this.props.dishes.erreMess}
 				/>
 				<RenderItem
 					item={this.props.promotions.promotions.filter(promo => promo.featured)[0]}
+					isLoading={this.props.promotions.isLoading}
+					erreMess={this.props.promotions.erreMess}
 				/>
 				<RenderItem
 					item={this.props.leaders.leaders.filter(leader => leader.featured)[0]}
+					isLoading={this.props.leaders.isLoading}
+					erreMess={this.props.leaders.erreMess}
 				/>
 			</ScrollView>
 		)
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -4,6 +4,7 @@ import { Tile } from 'react-native-elements'
 import { connect } from 'react-redux'
 
 import { baseUrl } from '../shared/baseUrl'
+import { Loading } from './LoadingComponent'
 
 const mapStateToProps = state => {
 	return {
@@ -31,13 +32,23 @@ class Menu extends Component {
 		}
 
 		const { navigate } = this.props.navigation
-		return (
-			<FlatList
-				data={this.props.dishes.dishes}
-				renderItem={renderMenuItem}
-				keyExtractor={item => item.id.toString()}
-			/>
-		)
+		if (this.props.dishes.isLoading) {
+			return <Loading />
+		} else if (this.props.dishes.errMess) {
+			return (
+				<View>
+					<Text>{this.props.dishes.errMess}</Text>
+				</View>
+			)
+		} else {
+			return (
+				<FlatList
+					data={this.props.dishes.dishes}
+					renderItem={renderMenuItem}
+					keyExtractor={item => item.id.toString()}
+				/>
+			)
+		}
 	}
 }
 
